c0f78afd6cc8378a94d7b7dc2c5b1d56f5d489af
DevilMuffin <alasdairjbw@gmail.com>
2024-08-10 02:21:43 +0100

    added icons, fixed music and made it cool

---
+++ assets/scripts/socials.js
@@ -0,0 +1,10 @@
+const socials = {
+    'github': "https://github.com/devilmuffin",
+    'youtube': "https://www.youtube.com/watch?v=dRH5wkfNQZ0",
+    'spotify': "https://open.spotify.com/user/31xma3hculmy7c24qdal5qi7h5fm",
+    'source': "https://github.com/DevilMuffin/DevilMuffin.github.io"
+}
+
+function openSocial(social) {
+    window.open(socials[social], "_blank");
+}
